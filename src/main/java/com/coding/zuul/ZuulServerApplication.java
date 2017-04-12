d921d48c5717a1490e64021ca232b55714b39a83
Steve Kuo <steve.kuo@makerstudios.com>
2017-04-12 15:01:24 -0700

    Remove everything but ZuulServerApplication.java

--- src/main/java/com/coding/zuul/ZuulServerApplication.java
+++ src/main/java/com/coding/zuul/ZuulServerApplication.java
@@ -1,17 +1,8 @@
 package com.coding.zuul;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
-
-import com.coding.zuul.utils.UserContextInterceptor;
 
 @SpringBootApplication
 @EnableZuulProxy
